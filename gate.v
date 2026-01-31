0824965c17ff04b2ed83a927cc1e9ea659360c5c
Jahnavi saddala <saddalajahnavireddy@gmail.com>
2026-01-31 09:45:28 +0530

    Add files via upload

---
+++ gate.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/19/2025 01:57:34 PM
+// Design Name: 
+// Module Name: gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gate(input a,input b,output y);
+
+   assign y=a&b;
+endmodule
